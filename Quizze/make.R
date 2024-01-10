4e6d01d2c71b95698987a65dc9f94a2c111f089e
luebby <karsten@statistix.org>
2024-01-10 19:33:25 +0100

    Ergänzung und Korrektur.

--- Quizze/make.R
+++ Quizze/make.R
@@ -9,8 +9,22 @@ Termin4 <- c("Frage4-1.Rmd", "Frage4-2.Rmd", "Frage4-3.Rmd", "Frage4-4.Rmd")
 Termin5 <- c("Frage5-1.Rmd", "Frage5-2.Rmd", "Frage5-3.Rmd", "Frage5-4.Rmd")
 Termin6 <- c("Frage6-1.Rmd", "Frage6-2.Rmd", "Frage6-3.Rmd", "Frage6-4.Rmd")
 Termin7 <- c("Frage7-1.Rmd", "Frage7-2.Rmd", "Frage7-3.Rmd", "Frage7-4.Rmd")
+Termin8 <- c("Frage8-1.Rmd", "Frage8-2.Rmd", "Frage8-3.Rmd", "Frage8-4.Rmd",
+             "Frage8-5.Rmd", "Frage8-6.Rmd", "Frage8-7.Rmd")
+Termin9 <- c("Frage9-1.Rmd", "Frage9-2.Rmd", "Frage9-3.Rmd", "Frage9-4.Rmd")
 
 # html
-exams2html(Termin7, converter = "pandoc-mathjax")
+exams2html(Termin9, converter = "pandoc-mathjax")
 # moodle
-exams2moodle(Termin7, name = "Wiederholung7", rule = "none")
+exams2moodle(Termin9, name = "Wiederholung9", rule = "none")
+
+
+# Alle
+Alle <- c(Termin1, Termin2, Termin3,
+          Termin4, Termin5, Termin6,
+          Termin7, Termin8, Termin9)
+length(Alle)
+# html
+exams2html(Alle, converter = "pandoc-mathjax")
+# moodle
+exams2moodle(Alle, name = "WiederholungAlle", rule = "none")
